11b8caef30ab2b27316273aaa56b58604828ca10
Filipe <filipefbn@gmail.com>
2020-05-12 17:35:24 -0300

    Testes de requisição e de session id para página principal

--- search_engine/aduna/tests.py
+++ search_engine/aduna/tests.py
@@ -4,21 +4,37 @@ from django.urls import reverse
 
 # Create your tests here.
 
-class SearchResultsTests(TestCase):
+class IndexTests(TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_request(self):
+       # Issue a GET request.
+       response = self.client.get(reverse('aduna:index'))
+
+       # Check that the response is 200 OK.
+       self.assertEqual(response.status_code, 200)
+
+    def test_generate_session_id(self):
+       # Issue a GET request.
+       response = self.client.get(reverse('aduna:index'))
+
+       # Check that we have a session id.
+       self.assertTrue(response.context['sid'])
+
+class SearchTests(TestCase):
 
     def setUp(self):
         # Every test needs a client.
         self.client = Client()
 
-    def test_total_docs_equal_docs(self):
+    def test_request(self):
         # Issue a GET request.
-        response = self.client.get(reverse('aduna:search'), {'query': 'query', 'page': 1, 'sid': 'sid', 'qid': ''})
+        response = self.client.get(reverse('aduna:search'), {'query': 'maria', 'page': 1, 'sid': 'sid', 'qid': ''})
 
         # Check that the response is 200 OK.
         self.assertEqual(response.status_code, 200)
-
-        # Check that the rendered context contains 0 results.
-        self.assertEqual(len(response.context['documents']), response.context['total_docs'])
 
     def test_query_no_results(self):
         # Issue a GET request.
@@ -36,7 +52,7 @@ class DocumentTests(TestCase):
         # Every test needs a client.
         self.client = Client()
 
-    def test_get_document(self):
+    def test_request(self):
        # Issue a GET request.
        response = self.client.get(reverse('aduna:document', kwargs={'doc_type': 'diario', 'doc_id': 'ZQejVnEBmEbG-Rbp6MAz'}))
 
